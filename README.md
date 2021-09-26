c6038d8510f88abbc9e4fea24f6516920bda6da4
Ananth <anantharajabk@gmail.com>
2021-09-27 00:06:26 +0530

    fix-error fixes

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Deployed in Netlify
   
 ## 🔗 Links
 [![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://anantharaja-portfolio.netlify.app/)
-[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/ananth-raj-0bb8b9176/)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/anantharaja-bhat-b-0bb8b9176/)
 [![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/Anantha67496007)
 
   
--- app.js
+++ app.js
@@ -5,58 +5,57 @@ var checkButton = document.querySelector("#check-button");
 var display = document.querySelector(".output");
 
 function percentageLoss(lossesBook, invested) {
+  var loss = (lossesBook / invested) * 100;
 
-    var loss = (lossesBook / invested) * 100;
-
-    return loss;
-
+  return loss;
 }
 
 function lossInPrice(invested, currentValue) {
+  var currentLoss = invested - currentValue;
 
-    var currentLoss = invested - currentValue;
-
-    return currentLoss;
+  return currentLoss;
 }
 
 function calculatePercentageGain(profitBook, invested) {
+  var percentGained = (profitBook / invested) * 100;
 
-    var percentGained = (profitBook / invested) * 100;
-
-    return percentGained;
-
+  return percentGained;
 }
 
 function changeInPrice(invested, currentValue) {
+  var difference = currentValue - invested;
 
-    var difference = currentValue - invested;
-
-    return difference;
-
+  return difference;
 }
 
 function stockPriceHandler() {
+  var invested = Number(purchasePrice.value) * Number(stockQuantity.value);
+  var currentValue = Number(currentPrice.value) * Number(stockQuantity.value);
+  var invested = Number(purchasePrice.value) * Number(stockQuantity.value);
+  var currentValue = Number(currentPrice.value) * Number(stockQuantity.value);
+  if (purchasePrice.value > 0 && stockQuantity.value > 0 && currentPrice.value > 0) {
+    if (Number(currentPrice.value) >= Number(purchasePrice.value)) {
+      var profitBook = changeInPrice(invested, currentValue);
 
-     var invested = Number(purchasePrice.value) * Number(stockQuantity.value);
-    var currentValue = Number(currentPrice.value) * Number(stockQuantity.value);
-    var invested = Number(purchasePrice.value) * Number(stockQuantity.value);
-    var currentValue = Number(currentPrice.value) * Number(stockQuantity.value);
+      var percentGained = calculatePercentageGain(profitBook, invested);
 
-    if ( Number(currentPrice.value) >= Number(purchasePrice.value)) {
-
-        var profitBook = changeInPrice(invested, currentValue);
-
-        var percentGained = calculatePercentageGain(profitBook, invested);
-
-        display.innerText = "Your profit is " + profitBook + " and Percent Gained " + percentGained + "%.";
-
+      display.innerText =
+        "Your profit is " +
+        profitBook +
+        " and Percent Gained " +
+        percentGained +
+        "%.";
     } else {
+      var lossesBook = lossInPrice(invested, currentValue);
+      var losses = percentageLoss(lossesBook, invested);
 
-        var lossesBook = lossInPrice(invested, currentValue);
-        var losses = percentageLoss(lossesBook, invested);
-
-         display.innerText = "You lossed  " + lossesBook + " and Percent Loss " + losses + "%.";
-
+      display.innerText =
+        "You lossed  " + lossesBook + " and Percent Loss " + losses + "%.";
     }
+  }
+  else {
+      display.innerText="Please enter the valid input"
+  }
 }
-checkButton.addEventListener('click', stockPriceHandler)
+
+checkButton.addEventListener("click", stockPriceHandler);
